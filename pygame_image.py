919468634767511cff9dc3f275f9bb45cbc7f0b1
fukasawa <c0a2421490@edu.teu.ac.jp>
2025-04-15 16:44:15 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -28,7 +28,9 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rect.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            kk_rect.move_ip((1, 0))
+            kk_rect.move_ip((2, 0))
+        
+        kk_rect.move_ip((-1, 0))
 
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
