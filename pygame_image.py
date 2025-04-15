ba1405f4d14c958467f080fba5ae4b9e711b46f0
fukasawa <c0a2421490@edu.teu.ac.jp>
2025-04-15 16:34:40 +0900

    練習10:完成

--- pygame_image.py
+++ pygame_image.py
@@ -14,15 +14,29 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img, True, False)
     tmr = 0
+    kk_rect =kk_img.get_rect()
+    kk_rect.center = 300, 200
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rect.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kk_rect.move_ip((0, 1))
+        if key_lst[pg.K_LEFT]:
+            kk_rect.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rect.move_ip((1, 0))
+
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x + 1600, 0])
         screen.blit(bg_img, [-x + 3200, 0])
-        screen.blit(kk_img, [300, 200])#4
+        screen.blit(kk_img, kk_rect)
+        #screen.blit(kk_img, [300, 200])#4
+
         pg.display.update()
         tmr += 1        
         clock.tick(200)#5
