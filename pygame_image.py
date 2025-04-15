8cc8a3eb1901ecd52838b941d0812e4a4f4daab8
fukasawa <c0a2421490@edu.teu.ac.jp>
2025-04-15 16:00:16 +0900

    練習8:反転

--- pygame_image.py
+++ pygame_image.py
@@ -12,6 +12,7 @@ def main():
     kk_img = pg.image.load("fig/3.png")#2
     kk_img = pg.transform.flip(kk_img, True, False)
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -19,7 +20,7 @@ def main():
         
         x = tmr
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [-x + 1600, 0])
+        screen.blit(bg_img2, [-x + 1600, 0])
         screen.blit(kk_img, [300, 200])#4
         pg.display.update()
         tmr += 1        
